bdba1844b134479d368d5fed478f05c403f29736
'sxl' <'839190458@qq.com'>
2017-09-28 15:43:24 +0800

    gjhkl;

--- yinxingshu/src/main/java/com/xhhy/service/MingxiServiceImpl.java
+++ yinxingshu/src/main/java/com/xhhy/service/MingxiServiceImpl.java
@@ -10,22 +10,25 @@ import com.github.pagehelper.PageInfo;
 import com.xhhy.bean.MingxiBean;
 import com.xhhy.bean.ResumeBean;
 import com.xhhy.dao.MingxiDao;
+
 @Service
 public class MingxiServiceImpl implements MingxiService {
 	@Autowired
-     private MingxiDao md;
-	public PageInfo queryAll(int pageNum,int pageSize,int num,MingxiBean mb) {
-		PageHelper.startPage(pageNum,pageSize);
+	private MingxiDao md;
+
+	public PageInfo queryAll(int pageNum, int pageSize, int num, MingxiBean mb) {
+		PageHelper.startPage(pageNum, pageSize);
 		List<MingxiBean> l = md.queryAll(mb);
-		PageInfo<Object> info = new PageInfo(l,num);
+		PageInfo<Object> info = new PageInfo(l, num);
 		return info;
 	}
-	//查询简历信息
-		public PageInfo  getqueryAll(int pageNum,int pageSize,int num,String user){
-			PageHelper.startPage(pageNum,pageSize);
-			List<ResumeBean> l=md.getqueryAll(user);
-			PageInfo<Object> info = new PageInfo(l,num);
-			return info;
-		}
+
+	// 查询简历信息
+	public PageInfo getqueryAll(int pageNum, int pageSize, int num, String user) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<ResumeBean> l = md.getqueryAll(user);
+		PageInfo<Object> info = new PageInfo(l, num);
+		return info;
+	}
 
 }
